964504603fc54f3a4e0133e0eaa1406c3f2dbf60
Lennart Keller <lennart.keller@uni-wuerzburg.de>
2021-11-20 14:30:58 +0100

    change sent tokenization

--- prepare_novels.py
+++ prepare_novels.py
@@ -1,32 +1,31 @@
 import json
 from pathlib import Path
-from nltk.tokenize.punkt import PunktSentenceTokenizer
+from nltk import sent_tokenize
 from tqdm.auto import tqdm
-
+from textspan import get_original_spans
+from more_itertools import flatten
 
 LANGUAGE = "german"
 SRC_PATH = Path("/home/keller/Uni/perry-rhodan-summarization/data/novells/")
 DST_PATH = Path("data/test/")
 
-def make_preparation_func():
-    sent_tokenizer = PunktSentenceTokenizer()
-    def prepare_novel(text):
-        text = " ".join(text.split())
-        sentences = [
-            {"begin": begin_idx, "end": end_idx}
-            for begin_idx, end_idx in sent_tokenizer.span_tokenize(text)
-            ]
-        data = { 
-            "text": text,
-            "sentences": sentences,
-            "scenes": [{"begin": 25, "end": 1423, "type": "Scene"}] # dummies...
-        }
-        return data
-    return prepare_novel
+
+
+def prepare_novel(text):
+    text = " ".join(text.split())
+    sentences = [
+        {"begin": begin_idx, "end": end_idx}
+        for begin_idx, end_idx in flatten(get_original_spans(sent_tokenize(text, language=LANGUAGE), text))
+    ]
+    data = { 
+        "text": text,
+        "sentences": sentences,
+        "scenes": [{"begin": 25, "end": 1423, "type": "Scene"}] # dummies...
+    }
+    return data
 
 
 if __name__ == "__main__":
-    prepare_novel = make_preparation_func()
     novels = list(SRC_PATH.glob("*.txt"))
     for novel in tqdm(novels):
         text = novel.read_text()
