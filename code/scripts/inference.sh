b0af3344bf37ea430fbf3a95c6058c4f4eee4967
Lennart Keller <lennart.keller@uni-wuerzburg.de>
2021-11-20 16:21:17 +0100

    use gpu for inference

--- code/scripts/inference.sh
+++ code/scripts/inference.sh
@@ -5,4 +5,4 @@ test_file=$2
 output_file=$3
 
 echo "Running the model on ${test_file}"
-python -m allennlp predict "${model}" "${test_file}" --output-file "${output_file}" --silent  --use-dataset-reader --predictor SeqClassificationInference --include-package code.sequential_sentence_classification
+python -m allennlp predict "${model}" "${test_file}" --output-file "${output_file}" --use-dataset-reader --predictor SeqClassificationInference --include-package code.sequential_sentence_classification --silent --cuda-device 2
